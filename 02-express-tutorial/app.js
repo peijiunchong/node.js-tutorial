6f663163c259d50116b851569fef64ef71407fb4
pcg7 <peijiun.chong@3ds.com>
2022-07-31 09:07:00 +0800

    tutorial

---
+++ 02-express-tutorial/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+// const path = require('path')
+const app = express()
+const people = require('./routes/people')
+const auth = require('./routes/auth')
+
+// static assets
+app.use(express.static('./methods-public'))
+// parse form data
+app.use(express.urlencoded({extended:false}))
+// parse json
+app.use(express.json())
+
+app.use('/api/people', people)
+
+app.use('/login', auth)
+
+app.listen(5000, ()=>{
+    console.log('server is listening on port 5000...');
+})
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
